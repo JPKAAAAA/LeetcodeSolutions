ca4e542df7c0405c2dd5ed48dc7df01f9f50f719
SURAJ_JHA <jha503856+git@gmail.com>
2023-08-20 23:15:25 +0530

    bit manipulation approach

---
+++ divideTwoIntegers.cpp
@@ -0,0 +1,34 @@
+//20-08-23
+
+
+#include<iostream>
+#include<limits.h>
+using namespace std;
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
+        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        while (dvd >= dvs) {
+            long temp = dvs, m = 1;
+            while (temp << 1 <= dvd) {
+                temp <<= 1;
+                m <<= 1;
+            }
+            dvd -= temp;
+            ans += m;
+        }
+        return sign * ans;
+    }
+};
+
+int main()
+{
+    system("cls");
+    Solution s;
+    cout<<s.divide(100,3);
+    return 0;
+}
